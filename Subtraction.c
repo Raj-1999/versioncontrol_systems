52cfc6cbd1ba7e4bb1abe12e56818d0dd40f3a76
Raj-1999 <singhrajshree479@gmail.com>
2020-02-29 16:01:56 +0530

    Done1

--- Subtraction.c
+++ Subtraction.c
@@ -1,16 +1,8 @@
 #include<stdio.h>
 void main()
 {
-<<<<<<< Updated upstream
-    int a,b,diff;
-    diff=a-b;
-    printf("a-b=%d",diff);
-    
-}
-=======
     int a,b,c;
     c=a-b;
     printf("a-b=%d",c);
     printf("Hello World");
 }
->>>>>>> Stashed changes
